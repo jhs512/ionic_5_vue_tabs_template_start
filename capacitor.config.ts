dd4fc0dec32e00db21cc68a7496272e8d56bcf35
jhs512 <jangka512@gmail.com>
2021-07-16 23:26:02 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Exam1',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
